c6e9b280d096368e534fb4728801410d0ac97954
Zander <zandervonn@gmail.com>
2023-04-25 23:33:37 +1200

    cleanup of card list card declerations 2

--- Assets/Scripts/Scriptables/CardsList.cs
+++ Assets/Scripts/Scriptables/CardsList.cs
@@ -28,266 +28,154 @@ public class CardsList : MonoBehaviour
         cards.Add(new ManaLeftCard());
     }
 
-    //private Card CreateBasicCard()
-    //{
-    //    float cardCost_multi = 0.4f;
-    //    float cardCost_fixed = 10f;
+    public class BasicCard : Card
+    {
+        public BasicCard()
+        {
+            cardName = "The Basic";
+            description = "Increase score by 1 x level";
+            artwork = Resources.Load<Sprite>("CardImages/Coin");
+            startingLevel = 1;
+            baseCardManaCost = 10;
+            rarity = 50;
 
-    //    Card basicCard = Card.CreateInstance(
-    //        "The Basic",
-    //        "Increase score by 1 x level",
-    //        Resources.Load<Sprite>("CardImages/Coin"),
-    //        null,
-    //        startingLevel,
-    //        baseCardManaCost,
-    //        50
-    //    );
+            CardCost = new ManaCardCost(
+                (cardInstance) => {
+                    float baseCost = 10f + (GameManager.Instance.mana * 0.4f);
+                    float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    return (float)(baseCost * approachZero);
+                }
+            );
 
-    //    basicCard.cardRewardType = CardValueType.Score;
-    //    basicCard.cardCostType = CardValueType.Mana;
+            CardReward = new ScoreCardReward(
+                (cardInstance) => {
+                    float rarityAdd = cardInstance.rarity / 10;
+                    return 10f + (cardInstance.level * (5f + rarityAdd));
+                }
+            );
+        }
+    }
 
-    //    FormulaDelegate costFormula = (cardInstance) => {
-    //        float baseCost = cardCost_fixed + (GameManager.Instance.mana * cardCost_multi);
-    //        float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-    //        return (float)(baseCost * approachZero);
-    //    };
-    //    basicCard.CostFormula = costFormula;
+    public class DoubleCard : Card
+    {
+        public DoubleCard()
+        {
+            cardName = "Multiply";
+            description = "x your score \n half your mana";
+            artwork = Resources.Load<Sprite>("CardImages/CoinBag");
+            startingLevel = 1;
+            baseCardManaCost = 40;
+            rarity = 1;
 
-    //    FormulaDelegate rewardFormula = (cardInstance) => {
-    //        //return 10f + (float)Math.Pow(1f + (cardInstance.level / 30f), 10); //f(x) = 1 + (1 + x/30)^10
-    //        float rarityAdd = cardInstance.rarity / 10; //1-10 x
-    //        return 10f + (cardInstance.level * (5f + rarityAdd)); //f(x) = x * 10, to keep the increase liniar
+            CardCost = new ManaCardCost(
+                (cardInstance) => {
+                    float baseCost = 40f;
+                    float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    return (float)(baseCost * approachZero);
+                }
+            );
 
-    //    };
-    //    basicCard.RewardFormula = rewardFormula;
+            CardReward = new ScoreCardReward(
+                (cardInstance) => {
+                    GameManager gameManager = FindObjectOfType<GameManager>();
+                    int startScore = gameManager.fieldScore;
+                    float approachInf = (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    float rarityAdd = 1 + (cardInstance.rarity) / 25;
+                    return startScore * approachInf * rarityAdd;
+                }
+            );
+        }
+    }
 
-    //    basicCard.Actions = new List<Action<GameManager, CardInstance>>
-    //    {
-    //        (gameManager, cardInstance) => {
-    //            float reward = rewardFormula(cardInstance);
-    //            gameManager.IncreaseScore((int) reward);
+    public class ManaResetCard : Card
+    {
+        public ManaResetCard()
+        {
+            cardName = "I NEED MORE TIME";
+            description = null;
+            artwork = Resources.Load<Sprite>("CardImages/Clock");
+            startingLevel = 1;
+            baseCardManaCost = 0;
+            rarity = 1;
 
-    //            float cost = costFormula(cardInstance);
-    //            gameManager.DecreaseMana((int) cost);
-    //        }
-    //    };
+            CardCost = new ScoreCardCost(
+                (cardInstance) => {
+                    GameManager gameManager = FindObjectOfType<GameManager>();
+                    int startScore = gameManager.fieldScore;
+                    float baseCost = 30f;
+                    float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    return (float)(startScore * ((1f / 100f) * baseCost * approachZero));
+                }
+            );
 
-        
-    //    basicCard.IsAffordable = (cardInstance, gameManager) => {
-    //        return gameManager.mana >= costFormula(cardInstance);
-    //    };
+            CardReward = new ManaCardReward(
+                (cardInstance) => {
+                    float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    float rarityAdd = 1 + (cardInstance.rarity / 50);
+                    return (100f - (80f * approachZero)) * rarityAdd;
+                }
+            );
+        }
+    }
 
-    //    return basicCard;
-    //}
+    public class DrawCardsCard : Card
+    {
+        public DrawCardsCard()
+        {
+            cardName = "Draw Cards";
+            description = null;
+            artwork = Resources.Load<Sprite>("CardImages/Cards");
+            startingLevel = 1;
+            baseCardManaCost = 30;
+            rarity = 1;
 
-    //private Card CreateDoubleCard()
-    //{
+            CardCost = new ManaCardCost(
+                (cardInstance) => {
+                    float baseCost = 30f;
+                    float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    return (float)(baseCost * approachZero);
+                }
+            );
 
-    //    float cardCost = 40f;
+            CardReward = new CardCardReward(
+                (cardInstance) => {
+                    float rarityAdd = 2 + (cardInstance.rarity / 25);
+                    return rarityAdd;
+                }
+            );
+        }
+    }
 
-    //    Card doubleCard = Card.CreateInstance(
-    //        "Multiply",
-    //        "x your score \n half your mana",
-    //        Resources.Load<Sprite>("CardImages/CoinBag"),
-    //        null,
-    //        startingLevel,
-    //        baseCardManaCost,
-    //        1
-    //    );
+    public class ManaLeftCard : Card
+    {
+        public ManaLeftCard()
+        {
+            cardName = "Mana Left";
+            description = "Multiply current mana, add it to score";
+            artwork = null;
+            startingLevel = 1;
+            baseCardManaCost = 10;
+            rarity = 1;
 
-    //    doubleCard.cardRewardType = CardValueType.Score;
-    //    doubleCard.cardCostType = CardValueType.Mana;
+            CardCost = new ManaCardCost(
+                (cardInstance) => {
+                    float baseCost = 10f;
+                    float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    return (float)(baseCost * approachZero);
+                }
+            );
 
-    //    FormulaDelegate costFormula = (cardInstance) => {
-    //        float baseCost = cardCost;
-    //        float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-    //        return (float)(baseCost * approachZero);
-    //    };
-    //    doubleCard.CostFormula = costFormula;
-
-    //    FormulaDelegate rewardFormula = (cardInstance) => {
-    //        GameManager gameManager = FindObjectOfType<GameManager>();
-    //        int startScore = gameManager.fieldScore;
-    //        float approachInf = (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = (1 + x/100)^speed
-    //        float rarityAdd = 1 + (cardInstance.rarity) / 25; //1-5 x
-    //        return startScore * approachInf * rarityAdd;
-    //    };
-    //    doubleCard.RewardFormula = rewardFormula;
-
-    //    doubleCard.Actions = new List<Action<GameManager, CardInstance>>
-    //    {
-    //        (gameManager, cardInstance) => {
-    //            float reward = rewardFormula(cardInstance); 
-    //            gameManager.IncreaseScore((int) reward);
-
-    //            float cost = costFormula(cardInstance);
-    //            gameManager.DecreaseMana((int) cost);
-    //        }
-    //    };
-
-    //    doubleCard.IsAffordable = (cardInstance, gameManager) => {
-    //        return gameManager.mana >= costFormula(cardInstance);
-    //    };
-
-    //    return doubleCard;
-    //}
-
-    //private Card CreateManaResetCard()
-    //{
-
-    //    float cardCost = 30f;
-
-    //    Card manaReset = Card.CreateInstance(
-    //        "I NEED MORE TIME",
-    //        null,
-    //        Resources.Load<Sprite>("CardImages/Clock"),
-    //        null,
-    //        startingLevel,
-    //        0,
-    //        1
-    //    );
-
-    //    manaReset.cardRewardType = CardValueType.Mana;
-    //    manaReset.cardCostType = CardValueType.Score;
-
-    //    FormulaDelegate costFormula = (cardInstance) => {
-    //        GameManager gameManager = FindObjectOfType<GameManager>();
-    //        int startScore = gameManager.fieldScore;
-    //        float baseCost = cardCost;
-    //        float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-            
-    //        return (float)(startScore * ((1f / 100f) * baseCost * approachZero)); //costs x % of score
-    //    };
-    //    manaReset.CostFormula = costFormula;
-
-    //    FormulaDelegate rewardFormula = (cardInstance) => {
-    //        float approachZero = 1f / (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-    //        float rarityAdd = 1 + (cardInstance.rarity / 50); // 1 - 3 x
-    //        return (100f - (80f * approachZero)) * rarityAdd; //20 > 22 > ... > 100
-
-    //    };
-    //    manaReset.RewardFormula = rewardFormula;
-
-    //    manaReset.Actions = new List<Action<GameManager, CardInstance>>
-    //    {
-    //        (gameManager, cardInstance) => {
-    //            float reward = rewardFormula(cardInstance);
-    //            gameManager.IncreaseMana((int) reward);
-
-    //            float cost = costFormula(cardInstance);
-    //            gameManager.DecreaseScore((int) cost);
-    //        }
-    //    };
-
-    //    manaReset.IsAffordable = (cardInstance, gameManager) => {
-    //        return gameManager.fieldScore >= costFormula(cardInstance);
-    //    };
-
-    //    return manaReset;
-    //}
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //private Card CreateDrawCardsCard()
-    //{
-    //    float cardCost = 30f;
-
-    //    Card drawCardsCard = Card.CreateInstance(
-    //        "Draw Cards",
-    //        null,
-    //        Resources.Load<Sprite>("CardImages/Cards"),
-    //        null,
-    //        startingLevel,
-    //        baseCardManaCost,
-    //        1
-    //    );
-
-    //    drawCardsCard.cardRewardType = CardValueType.Card;
-    //    drawCardsCard.cardCostType = CardValueType.Mana;
-
-    //    FormulaDelegate costFormula = (cardInstance) => {
-    //        float baseCost = cardCost;
-    //        float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-    //        return (float)(baseCost * approachZero);
-    //    };
-    //    drawCardsCard.CostFormula = costFormula;
-
-    //    FormulaDelegate rewardFormula = (cardInstance) => {
-    //        float rarityAdd = 2 + (cardInstance.rarity / 25); // 2 - 6
-    //        return rarityAdd;
-
-    //    };
-    //    drawCardsCard.RewardFormula = rewardFormula;
-
-    //    drawCardsCard.Actions = new List<Action<GameManager, CardInstance>>
-    //    {
-    //        (gameManager, cardInstance) => {
-    //            float reward = rewardFormula(cardInstance);
-    //            DrawCard drawCard = FindObjectOfType<DrawCard>();
-    //            drawCard.DrawCards((int) reward);
-
-    //            float cost = costFormula(cardInstance);
-    //            gameManager.DecreaseMana((int) cost);
-    //        }
-    //    };
-
-    //    drawCardsCard.IsAffordable = (cardInstance, gameManager) => {
-    //        return gameManager.mana >= costFormula(cardInstance);
-    //    };
-
-    //    return drawCardsCard;
-    //}
-
-    //private Card CreateManaLeftCard()
-    //{
-    //    float cardCost = 10f;
-
-    //    Card manaLeftCard = Card.CreateInstance(
-    //        "Mana Left",
-    //        "Multiply current mana, add it to score",
-    //        null,
-    //        null,
-    //        startingLevel,
-    //        baseCardManaCost,
-    //        1
-    //    );
-
-    //    manaLeftCard.cardRewardType = CardValueType.Score;
-    //    manaLeftCard.cardCostType = CardValueType.Mana;
-
-    //    FormulaDelegate costFormula = (cardInstance) => {
-    //        float baseCost = cardCost;
-    //        float approachZero = (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = 1/(1 + x/100)^speed
-    //        return (float)(baseCost * approachZero);
-    //    };
-    //    manaLeftCard.CostFormula = costFormula;
-
-    //    FormulaDelegate rewardFormula = (cardInstance) => {
-    //        GameManager gameManager = FindObjectOfType<GameManager>();
-    //        float mana = gameManager.mana;
-    //        float approachInf = (float)Math.Pow(1f + cardInstance.level / 100f, speedToZero); // f(x) = (1 + x/100)^speed
-    //        float rarityAdd = 1 + (cardInstance.rarity) / 25; //1-5 x
-    //        return (mana * approachInf * rarityAdd) / 10 ;
-    //    };
-    //    manaLeftCard.RewardFormula = rewardFormula;
-
-    //    manaLeftCard.Actions = new List<Action<GameManager, CardInstance>>
-    //    {
-    //        (gameManager, cardInstance) => {
-    //            float reward = rewardFormula(cardInstance);
-    //            gameManager.IncreaseScore((int) reward);
-
-    //            float cost = costFormula(cardInstance);
-    //            gameManager.DecreaseMana((int) cost);
-    //        }
-    //    };
-
-    //    manaLeftCard.IsAffordable = (cardInstance, gameManager) => {
-    //        return gameManager.mana >= costFormula(cardInstance);
-    //    };
-
-    //    return manaLeftCard;
-    //}
+            CardReward = new ScoreCardReward(
+                (cardInstance) => {
+                    GameManager gameManager = FindObjectOfType<GameManager>();
+                    float mana = gameManager.mana;
+                    float approachInf = (float)Math.Pow(1f + cardInstance.level / 100f, 3);
+                    float rarityAdd = 1 + (cardInstance.rarity) / 25;
+                    return (mana * approachInf * rarityAdd) / 10;
+                }
+            );
+        }
+    }
 
 }
 
